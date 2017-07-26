956e3cca8a1b424029f6e483dc06e01e8dcfc7eb
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2017-07-26 10:03:33 -0700

    Added rebuild test

--- scripts/tests/test_pdb_structure.py
+++ scripts/tests/test_pdb_structure.py
@@ -28,10 +28,30 @@ class TestPDBFile(unittest.TestCase):
 
         pdb_file.clean_waters()
         self.assertEqual(len(pdb_file.models[0].atoms), 7993, msg="Should prune 394 waters")
-        self.assertFalse([ a for a in pdb_file.models[0].atoms if a.resn == "HOH"])
+        self.assertFalse([a for a in pdb_file.models[0].atoms if a.resn == "HOH"])
 
         nearby = pdb_file.models[0].get_nearby(near_water.pos, 1.8)
         self.assertEqual(len(nearby), 2)  # Now only near itself and CA
+
+    def test_rebuild_atomids(self):
+        pdb_file = PDBFile("5udi.pdb")
+        first_atom = pdb_file.models[0].get_atom("A", 8, "N")
+        last_atom = pdb_file.models[0].get_atom("B", 125, "O")
+        self.assertEqual(first_atom.id, 1)
+        self.assertEqual(last_atom.id, 8389)
+
+        pdb_file.rebuild_atomids()
+
+        self.assertEqual(first_atom.id, 1)
+        self.assertEqual(last_atom.id, 8387)
+
+        pdb_file.clean_waters()
+        pdb_file.rebuild_atomids()
+
+        self.assertEqual(first_atom.id, 1)
+        self.assertEqual(last_atom.id, 8387)
+        last_atom = pdb_file.models[0].atoms[-1]
+        self.assertEqual(last_atom.id, 7993)
 
 
 if __name__ == '__main__':
